51e80a82097fd94a9b25870a78462181b569356c
yellowsubmarine1447 <cyril@unswcpmsoc.com>
2024-04-05 16:12:36 +1100

    refactor: increase sampling size

--- analysis.py
+++ analysis.py
@@ -1,21 +1,28 @@
-# takes a few seconds (like 10)
+# takes like a minute or two with the provided settings
 
 from scipy.optimize import curve_fit
 from random import randint
 from subprocess import check_output
 from os import system
 import matplotlib.pyplot as plt
+from sys import argv
+
 LEFT = 100
-RIGHT = 500
+RIGHT = 600
+SAMPLING_DIST = 20
 
 def cubic(x, a, b, c, d):
-    y = a * x** 3 + b * x ** 2 + c * x + d
+    y = a * x** 3 + b * x**2 + c*x + d
     return y
 
 ydata = []
 xdata = []
-system("g++ random_tree_algo.cpp worst_case_expected.cpp")
-for i in range(LEFT, RIGHT, 10):
+if len(argv) < 2:
+    print("please provide .cpp file that generates random graph")
+    exit(1)
+
+system(f"g++ random_tree_algo.cpp {argv[1]}")
+for i in range(LEFT, RIGHT, SAMPLING_DIST):
     s = 0
     for _ in range(20):
         s += int(check_output(["./a.out", str(i)]))
--- random_graph.cpp
+++ random_graph.cpp
@@ -7,28 +7,11 @@ using namespace std;
 // n >= 4 pls
 vector<vector<int>> create_graph(int N) {
     vector<vector<int>> adj(N);
-    int chain_length = N / 4;
-    adj[0].push_back(1);
-    adj[N-1].push_back(N-2);
-    adj[chain_length].push_back(chain_length-1);
-    adj[N-chain_length-1].push_back(N-chain_length);
-
-    for (int i = 1; i < chain_length; i++) {
-        adj[i].push_back(i-1);
-        adj[i].push_back(i+1);
-    }
-
-    for (int i = N - chain_length; i < N-1; i++) {
-        adj[i].push_back(i-1);
-        adj[i].push_back(i+1);
-    }
-
-    for (int i = chain_length; i < N - chain_length; i++) {
-        for (int j = chain_length; j < N - chain_length; j++) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
             if (i != j) {
                 adj[i].push_back(j);
             }
         }
     }
-    return adj;
 }
