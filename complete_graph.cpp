d9f8e3719b057b4e1360e0b53db50427f12c076e
yellowsubmarine1447 <cyril@unswcpmsoc.com>
2024-04-05 16:33:30 +1100

    new; more trees

---
+++ complete_graph.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+vector<vector<int>> create_graph(int N) {
+    vector<vector<int>> adj(N);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i != j) {
+                adj[i].push_back(j);
+            }
+        }
+    }
+}
