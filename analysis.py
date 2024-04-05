848e9ba8584044d36f15a352d56021adf455b8b3
yellowsubmarine1447 <cyril@unswcpmsoc.com>
2024-04-05 22:27:39 +1100

    refactor: interpolated functions write to png

--- analysis.py
+++ analysis.py
@@ -12,7 +12,7 @@ RIGHT = 600
 SAMPLING_DIST = 20
 
 def cubic(x, a, b, c, d):
-    y = a * x** 3 + b * x**2 + c*x + d
+    y = a * x ** 3 + b * x ** 2 + c * x + d
     return y
 
 ydata = []
@@ -21,7 +21,7 @@ if len(argv) < 2:
     print("please provide .cpp file that generates random graph")
     exit(1)
 
-system(f"g++ call_count.cpp {argv[1]}")
+system(f"g++ call_count.cpp wilsons_algo.cpp {argv[1]}")
 for i in range(LEFT, RIGHT, SAMPLING_DIST):
     s = 0
     for _ in range(20):
@@ -30,6 +30,6 @@ for i in range(LEFT, RIGHT, SAMPLING_DIST):
     xdata.append(i)
 
 a, b, c, d =  curve_fit(cubic, xdata, ydata)[0]
-plt.plot(range(LEFT, RIGHT), [cubic(x, a,b, c, d) for x in range(LEFT, RIGHT)])
+plt.plot(range(LEFT, RIGHT), [cubic(x, a, b, c, d) for x in range(LEFT, RIGHT)])
 plt.scatter(xdata, ydata)
-plt.show()
+plt.savefig("cubic_fit.png")
