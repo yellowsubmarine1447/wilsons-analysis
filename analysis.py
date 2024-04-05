dd51c90000241f58e64e2300ea532ab31f93e1dc
yellowsubmarine1447 <cyril@unswcpmsoc.com>
2024-04-05 15:29:24 +1100

    feat: graph time complexity

---
+++ analysis.py
@@ -0,0 +1,28 @@
+# takes a few seconds
+
+from scipy.optimize import curve_fit
+from random import randint
+from subprocess import check_output
+from os import system
+import matplotlib.pyplot as plt
+LEFT = 100
+RIGHT = 200
+
+def cubic(x, a, b, c, d):
+    y = a * x** 3 + b * x ** 2 + c * x + d
+    return y
+
+ydata = []
+xdata = []
+system("g++ random_tree_algo.cpp worst_case_expected.c")
+for i in range(LEFT, RIGHT):
+    s = 0
+    for _ in range(20):
+        s += int(check_output(["./a.out", str(i)]))
+    ydata.append(s // 10)
+    xdata.append(i)
+
+a, b, c, d =  curve_fit(cubic, xdata, ydata)[0]
+plt.plot(range(LEFT, RIGHT), [cubic(x, a,b, c, d) for x in range(LEFT, RIGHT)])
+plt.scatter(xdata, ydata)
+plt.show()
