00a7b81b93a3b6e9db86d5b6ebedf0253ffe6d97
yellowsubmarine1447 <cyril@unswcpmsoc.com>
2024-04-05 16:45:53 +1100

    chore: rename file to wilsons algo

--- dist_analyser.py
+++ dist_analyser.py
@@ -3,7 +3,7 @@ from os import system
 dist = {}
 TRIALS = 1000
 
-system("g++ show_tree.cpp random_tree_algo.cpp complete_graph.cpp")
+system("g++ show_tree.cpp wilsons_algo.cpp complete_graph.cpp")
 for _ in range(TRIALS):
     result = check_output(["./a.out", "5"])
     if result not in dist:
